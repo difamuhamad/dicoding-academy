ff8f5a15bb1cbf7ef19bb8c3b5a91c1c8a7d30c1
difaMuhamad <difamuhamad06@gmail.com>
2025-06-04 09:41:11 +0700

    frontend-3-9

---
+++ citycareapp-deployment-final/dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,7 @@
+
+export const connectorConfig = {
+  connector: 'default',
+  service: '13-citycareapp-deployment-ready',
+  location: 'us-central1'
+};
+
---
+++ citycareapp-deployment-final/dataconnect-generated/js/default-connector/index.cjs.js
@@ -0,0 +1,8 @@
+
+const connectorConfig = {
+  connector: 'default',
+  service: '13-citycareapp-deployment-ready',
+  location: 'us-central1'
+};
+exports.connectorConfig = connectorConfig;
+
